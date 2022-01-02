56db9d9505d4a28ae59f789406f3c37adf52a824
Tim <timstanley1985@gmail.com>
2022-01-02 10:23:38 +0000

    Add custom commandline options and bump version

--- ebusd/CHANGELOG.md
+++ ebusd/CHANGELOG.md
@@ -1,5 +1,10 @@
 <!-- https://developers.home-assistant.io/docs/add-ons/presentation#keeping-a-changelog -->
 
+## 0.84
+
+- IMPROVE: Add MQTT retain option
+- IMPROVE: Add custom commandline options
+
 ## 0.81
 
 - FIX: null values were not accepted for some options
--- ebusd/run.sh
+++ ebusd/run.sh
@@ -12,13 +12,19 @@ do
 done
 
 #other options
-declare options=( "device" "port" "latency" "logareas" "loglevel" "mqtthost" "mqttport" "mqttuser" "mqttpass")
+declare options=( "device" "port" "latency" "logareas" "loglevel" "mqtthost" "mqttport" "mqttuser" "mqttpass" )
 for optName in "${options[@]}"
 do
     if ! bashio::config.is_empty ${optName}; then
         ebusd_args+=("--${optName}=$(bashio::config ${optName})")
     fi
 done
+
+#add additional options
+if ! bashio::config.is_empty commandline_options; then
+    ebusd_args+=("$(bashio::config commandline_options)")
+fi
+
 
 if bashio::config.false "foreground" || bashio::config.is_empty "foreground"; then
     bashio::config.suggest.true "foreground" "ebusd add-on will stop if ebusd is not running in the foreground."
